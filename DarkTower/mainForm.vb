a8037091e38c52df5f567ad44b8047039b7f6485
Jason <secfordoc23@gmail.com>
2019-02-17 21:47:45 -0600

    Initial Commit

--- DarkTower/mainForm.vb
+++ DarkTower/mainForm.vb
@@ -8,36 +8,15 @@ Option Strict On
 Option Explicit On
 
 Public Class mainForm
-    Private Box(11, 11) As PictureBox
-    '==========================================================================================
-    'Name: BuildMap
-    'Date: 
-    'Author: Jason Welch
-    'Purpose: 
-    Private Sub BuildMap()
-        For rowShort As Short = 0S To 11S
-            For columnShort As Short = 0S To 11S
-                DrawBox(rowShort, columnShort)
-            Next columnShort
-        Next rowShort
-    End Sub
-    '==========================================================================================
-    'Name: DrawBox
-    'Date: 
-    'Author: Jason Welch
-    'Purpose: 
-    Private Sub DrawBox(rowShort As Short, columnShort As Short)
-        Box(rowShort, columnShort) = New PictureBox
-        Box(rowShort, columnShort).Size = CType(New Point(50, 50), Size)
-        Box(rowShort, columnShort).BorderStyle = BorderStyle.FixedSingle
 
-    End Sub
     '==========================================================================================
     'Name: mainForm_Load
     'Date: 
     'Author: Jason Welch
     'Purpose: 
     Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        BuildMap()
+        'splashForm.ShowDialog()
+        'Check For Database
+
     End Sub
 End Class
