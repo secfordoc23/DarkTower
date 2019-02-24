f41493e040e0677a088c41d3d286e1be07c5006e
secfordoc23@gmail.com <secfordoc23@gmail.com>
2019-02-24 14:41:31 -0600

    Created PlayerTrun class

---
+++ DarkTower/PlayerTurn.vb
@@ -0,0 +1,88 @@
+﻿'Program: DarkTower
+'Me: PlayerTurn.vb
+'Date: TBD
+'Author: Jason Welch
+'Purpose: A class that controls the events of a Player taking there trun
+
+Option Strict On
+Option Explicit On
+
+Public Class PlayerTurn
+    Private userInventory As Inventory()
+
+    '==========================================================================================
+    'Name: Contructor
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: Default Constructor 
+    Public Sub New(ByVal currentInventory As Inventory())
+        userInventory = currentInventory
+    End Sub
+    '==========================================================================================
+    'Name: TakeATurn
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: Generate a Random Event 
+    Public Sub TakeATurn(ByVal moveType As Short)
+        Select Case moveType
+            Case 0 ' Territory
+                GenerateRandomEvent()
+            Case 1 ' Crypt
+            Case 2 ' Dark Tower
+            Case Else
+                MsgBox("Move Type not Recognized!", MsgBoxStyle.Critical, "Move Type Error")
+        End Select
+    End Sub
+    '==========================================================================================
+    'Name: RandomEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: Generate a Random Event 
+    Private Function GenerateRandomEvent() As String
+        Dim rand As New Random
+        Dim randomShort As Short
+
+        randomShort = CShort(rand.Next(100) + 1)
+
+        Select Case randomShort
+            Case 1 To 20
+                Return "Plague"
+            Case 40 To 60
+                Return "Lost"
+            Case 80 To 100
+                Return "Attacked"
+            Case Else
+                Return "Success"
+        End Select
+    End Function
+    '==========================================================================================
+    'Name: CryptEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose:  
+
+    '==========================================================================================
+    'Name: DarkTowerEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose:  
+
+    '==========================================================================================
+    'Name: PlagueEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: 
+
+    '==========================================================================================
+    'Name: LostEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: 
+
+    '==========================================================================================
+    'Name: AttackEvent
+    'Date: 2/19/19
+    'Author: Jason Welch
+    'Purpose: 
+End Class
+'================================== No Code Follows ===========================================
--- DarkTower/gamePlayModule.vb
+++ DarkTower/gamePlayModule.vb
@@ -10,36 +10,6 @@ Option Explicit On
 Module gamePlayModule
     Public currentInventory As Inventory
 
-    '==========================================================================================
-    'Name: RandomEvent
-    'Date: 2/19/19
-    'Author: Jason Welch
-    'Purpose: Generate a Random Event on Each Turn
-    Public Function RandomEvent() As String
-        Dim rand As New Random
-        Dim randomShort As Short
 
-        randomShort = CShort(rand.Next(100) + 1)
-
-        Select Case randomShort
-            Case 1 To 20
-                Return "Plague"
-            Case 40 To 60
-                Return "Lost"
-            Case 80 To 100
-                Return "Attacked"
-            Case Else
-                Return "Success"
-        End Select
-    End Function
-
-    '==========================================================================================
-    'Name: TakeATurn
-    'Date: 2/19/19
-    'Author: Jason Welch
-    'Purpose: Generate a Random Event
-    Public Sub TakeATurn()
-
-    End Sub
 End Module
 '================================== No Code Follows ===========================================
